6a9e59c4e4d210ecfdd9a4edf6680cdc33f27b75
sonshn <sonshn@naver.com>
2023-11-30 10:06:43 +0900

    sh: Bike(고장 신고)

--- src/main/java/com/spring/databasebike/constant/BaseResponseStatus.java
+++ src/main/java/com/spring/databasebike/constant/BaseResponseStatus.java
@@ -1,2 +1,35 @@
-package com.spring.databasebike.config;public enum BaseResponseStatus {
+package com.spring.databasebike.constant;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum BaseResponseStatus {
+    /**
+     * 200 : 요청 성공
+     */
+    SUCCESS(true, HttpStatus.OK.value(), "요청에 성공하였습니다."),
+
+    /**
+     * 400 : Request 오류, Response 오류
+     */
+    // Common
+    REQUEST_ERROR(false, HttpStatus.BAD_REQUEST.value(), "입력값을 확인해주세요."),
+    RESPONSE_ERROR(false, HttpStatus.NOT_FOUND.value(), "값을 불러오는데 실패하였습니다."),
+
+    /**
+     * 500 : Database, Server 오류
+     */
+    DATABASE_ERROR(false, HttpStatus.INTERNAL_SERVER_ERROR.value(), "데이터베이스 연결에 실패하였습니다."),
+    SERVER_ERROR(false, HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버와의 연결에 실패하였습니다.");
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
 }
--- src/main/java/com/spring/databasebike/domain/bike/entity/ReportBikeReq.java
+++ src/main/java/com/spring/databasebike/domain/bike/entity/ReportBikeReq.java
@@ -1,2 +1,12 @@
-package com.spring.databasebike.domain.bike.entity;public class ReportBikeReq {
+package com.spring.databasebike.domain.bike.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ReportBikeReq {
+    private String bike_id;
 }
